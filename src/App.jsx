c0f1c1779ae42eeb8aa1207324e54a44f0dc65d9
Nydali <Saints2812@gmail.com>
2024-08-19 14:44:29 +0200

    API

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Movie from './Movie';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Movie Information</h1>
+      <Movie />
+    </div>
+  );
+}
+
+export default App;
+
